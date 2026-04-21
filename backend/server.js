a2a2dca5137d2de65d7dcce556209feae40721ba
David Thairu <thaividz2@gmail.com>
2026-04-21 10:36:44 +0300

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -178,6 +178,8 @@ app.get("/bookings/per-day", async (req, res) => {
 });
 
 // START SERVER
-app.listen(5000, () => {
-  console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
